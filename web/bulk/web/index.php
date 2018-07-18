8fe94cfc12a65f2c1814ec5acd14739771a88ad4
SNB01\Ciaran <ciaran@smithey.com.au>
2018-07-19 06:04:13 +1000

    Added extra bulk actions

--- web/bulk/web/index.php
+++ web/bulk/web/index.php
@@ -25,7 +25,11 @@ if ($_SESSION['user'] == 'admin') {
             break;
         case 'enable-letsencrypt': $cmd="v-add-letsencrypt-domain";
             break;
-        case 'disable-letsencrypt': $cmd="v-delete-web-domain-ssl";
+        case 'disable-letsencrypt': $cmd="v-delete-letsencrypt-domain";
+            break;
+        case 'ssl-home-html': $cmd="ssl-home-html";
+            break;
+        case 'ssl-home-shtml': $cmd="ssl-home-shtml";
             break;
         default: header("Location: /list/web/"); exit;
     }
@@ -39,7 +43,42 @@ if ($_SESSION['user'] == 'admin') {
 
 foreach ($domain as $value) {
     $value = escapeshellarg($value);
-    exec (VESTA_CMD.$cmd." ".$user." ".$value." no", $output, $return_var);
+
+    if ($cmd == "v-add-letsencrypt-domain") {
+
+      exec (VESTA_CMD."v-list-web-domain ".$user." ".$value." json", $output, $return_var);
+      $data = json_decode(implode('', $output), true);
+      unset($output);
+      $v_aliases = str_replace(',', "\n", $data[$value]['ALIAS']);
+
+      $l_aliases = str_replace("\n", ',', $v_aliases);
+      exec (VESTA_CMD."v-add-letsencrypt-domain ".$user." ".$value." '".$l_aliases."' 'no'", $output, $return_var);
+      check_return_code($return_var,$output);
+      unset($output);
+
+    } elseif ($cmd == "v-delete-letsencrypt-domain") {
+
+      exec (VESTA_CMD."v-delete-web-domain-ssl ".$user." ".$value." 'no'", $output, $return_var);
+      check_return_code($return_var,$output);
+      unset($output);
+
+      exec (VESTA_CMD."v-delete-letsencrypt-domain ".$user." ".$value." 'no'", $output, $return_var);
+      check_return_code($return_var,$output);
+      unset($output);
+
+    } elseif ($cmd == "ssl-home-html") {
+      exec (VESTA_CMD."v-change-web-domain-sslhome ".$user." ".$value." same 'no'", $output, $return_var);
+      check_return_code($return_var,$output);
+      unset($output);
+
+    } elseif ($cmd == "ssl-home-shtml") {
+      exec (VESTA_CMD."v-change-web-domain-sslhome ".$user." ".$value." single 'no'", $output, $return_var);
+      check_return_code($return_var,$output);
+      unset($output);
+
+    } else {
+      exec (VESTA_CMD.$cmd." ".$user." ".$value." no", $output, $return_var);
+    }
     $restart='yes';
 }
 
